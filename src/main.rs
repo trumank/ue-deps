979a22a00cb173f73a0f035c08928f9a7061a803
Truman Kilen <t@kilen.me>
2023-05-08 12:27:38 -0500

    Better error handling and allow accept multiple engine paths

--- src/main.rs
+++ src/main.rs
@@ -19,25 +19,34 @@ impl Dependency {
 
 fn main() {
     let args = std::env::args().collect::<Vec<_>>();
-    match args.len() {
-        3 => {
-            match args[1].as_str() {
-                "cache" => {
-                    return build_cache(PathBuf::from(&args[2]));
-                },
-                "restore" => {
-                    return restore_cache(PathBuf::from(&args[2]));
-                },
-                _ => { }
-            }
-        }
-        _ => { }
+    if !Path::new("deps_cache").exists() {
+        println!("./deps_cache directory does not exit");
+        return;
     }
-    println!("usage: [cache/restore] [unreal engine root]")
+    if args.len() >= 3 {
+        match args[1].as_str() {
+            "cache" => {
+                for path in &args[2..] {
+                    println!("caching {}", path);
+                    build_cache(PathBuf::from(path));
+                }
+                return
+            },
+            "restore" => {
+                for path in &args[2..] {
+                    println!("restoring {}", path);
+                    return restore_cache(PathBuf::from(path));
+                }
+                return
+            },
+            _ => { }
+        }
+    }
+    println!("usage: [cache/restore] [unreal engine root dirs...]")
 }
 
 fn restore_cache<P: AsRef<Path>>(ue: P) {
-    let cache = PathBuf::from("../deps_cache");
+    let cache = PathBuf::from("deps_cache");
     let config = std::fs::read_to_string(ue.as_ref().join(".ue4dependencies")).unwrap();
     let xml = roxmltree::Document::parse(&config).unwrap();
 
@@ -47,8 +56,6 @@ fn restore_cache<P: AsRef<Path>>(ue: P) {
     use rayon::prelude::*;
     let root = PathBuf::from(ue.as_ref());
     dependencies.into_par_iter().for_each(|dep| {
-        //bar.println(&dep.name);
-
         let dep_path = root.join(&dep.name);
 
         let restore = if cache.join(&dep_path).exists() {
@@ -80,7 +87,7 @@ fn restore_cache<P: AsRef<Path>>(ue: P) {
 }
 
 fn build_cache<P: AsRef<Path>>(ue: P) {
-    let cache = PathBuf::from("../deps_cache");
+    let cache = PathBuf::from("deps_cache");
     let config = std::fs::read_to_string(ue.as_ref().join(".ue4dependencies")).unwrap();
     let xml = roxmltree::Document::parse(&config).unwrap();
 
@@ -89,22 +96,30 @@ fn build_cache<P: AsRef<Path>>(ue: P) {
 
     use rayon::prelude::*;
     let root = PathBuf::from(ue.as_ref());
-    dependencies.into_par_iter().for_each(|dep| {
+
+    let cache = |dep: &Dependency| -> Result<(), std::io::Error> {
         //bar.println(&dep.name);
 
         if !cache.join(&dep.expected_hash).exists() {
             use crypto::digest::Digest;
             use crypto::sha1::Sha1;
 
-            let bytes = std::fs::read(root.join(dep.name)).unwrap();
+            let bytes = std::fs::read(root.join(&dep.name))?;
             let mut hasher = Sha1::new();
             hasher.input(&bytes);
             let hash = hasher.result_str();
             if hash == dep.expected_hash {
                 let tmp = cache.join(format!(".{}", hash));
-                std::fs::write(&tmp, &bytes).unwrap();
-                std::fs::rename(&tmp, cache.join(hash)).unwrap();
+                std::fs::write(&tmp, &bytes)?;
+                std::fs::rename(&tmp, cache.join(hash))?;
             }
+        }
+        Ok(())
+    };
+
+    dependencies.into_par_iter().for_each(|dep| {
+        if let Err(e) = cache(&dep) {
+            bar.println(format!("error caching {}: {}", root.join(dep.name).display(), e));
         }
         bar.inc(1);
     });
